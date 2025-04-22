1d0df72fb86e2a55b0fd4503a57a8a6a37e2e020
geanCarneiro <geancarneiro1@gmail.com>
2025-04-22 15:49:44 -0300

    initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var jQueryCDN = document.createElement('script');
+jQueryCDN.src = 'https://code.jquery.com/jquery-3.7.1.js';
+jQueryCDN.integrity = 'sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4='
+jQueryCDN.crossOrigin = 'anonymous'
+
+document.querySelector('head').appendChild(jQueryCDN);
+
